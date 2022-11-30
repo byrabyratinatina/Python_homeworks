7cdf4067a9dc1b1ce826fa28768231e6f27e7488
Андрей Владимирович Майоров <113131162+byrabyratinatina@users.noreply.github.com>
2022-11-30 12:35:39 +0300

    Delete task4.py

--- session2/task4.py
+++
@@ -1,9 +0,0 @@
-people = int(input('Количество человек в кругу: '))
-count = int(input('Какое число счета (для выбывания)? '))
-list_people = list(range(1, people + 1))
-out_ind = 0 
-for i in range(people - 1):
-    start_ind = out_ind % len(list_people)    
-    out_ind = (start_ind + count- 1) % len(list_people)
-    list_people.remove(list_people[out_ind])
-print('Остался человек под номером', list_people[0])
